b774aa78fb5dafb7e98fca7bd604f98bf0ccf0fc
Matias Lopez Cortese <matais.lopez.89@hotmail.com>
2023-05-27 18:55:56 -0300

    primera entrega

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 let clave = "1234";
 
 function loguear (){
@@ -21,24 +22,87 @@ function loguear (){
 
 if (loguear()){
 
-function compra(valor) {
-    if(valor>10000){
-        valor-descuento1
-        return (valor-descuento1)
 
-    }else if(valor<=10000 && valor>=5000){
-        valor-descuento2
-        return (valor-descuento2)
 
-    }else(valor<5000)
-      
-        return(valor)
+const juegos = [
+{id:1,nombre:"Elden Ring",genero:"rpg", precio:2100, disponible:true},
+{id:2,nombre:"Sekiro",genero:"rpg", precio:2200, disponible:true},
+{id:3,nombre:"Warzone",genero:"shooter", precio:2300, disponible:true},
+{id:4,nombre:"Gta",genero:"aventura", precio:2400,disponible:true},
+{id:5,nombre:"Red Dead",genero:"aventura", precio:2500, disponible:true},
+];
+
+let carrito = []
+
+let seleccion = prompt("Seleccion Opción \n 1 - comprar juegos \n 2 -  buscar juego ");
+
+
+if(seleccion == "1"){
+let juegosDisponibles = juegos.map(
+    (juegos)=> juegos.nombre + " $" +juegos.precio
+);
+alert (juegosDisponibles.join("\n"))
+} else(seleccion == "2")
+
+
+if(seleccion == 1){
+    let juego = prompt("Selecciona el juego a comprar")
+    let precio = 0
+
+    if(juego == "Elden Ring" || juego == "Sekiro" || juego == "Warzone"|| juego == "Gta"|| juegoo == "Red Dead"){
+        switch (juego) {
+            case "Elden Ring":
+             precio = 2100;   
+             break;
+            case "Sekiro":
+             precio = 2200;  
+                break;
+            case "Warzone":
+                precio = 2300;
+             break;
+            case "Gta":
+                precio = 2400;
+             break;
+            case "Red Dead":
+                precio = 2500;
+                break;
+
+            default:
+            break;
+        }
+        carrito.push({juego,precio})
+        console.log(carrito)
+    }else{
+       alert("juego no encontrado")
+    }
+   
+    carrito.forEach((carritoFinal) => {
+    alert(`juego: ${carritoFinal.juego}, total a pagar: ${carritoFinal.precio}`)
+    })    
+    }
+    
+   
+
+
+
+function  stockJuegos (stock, nombre) {
+    return stock.filter (p => p.nombre == nombre);
 }
 
 
-let valor = Number(prompt("Ingrese compra"));
-let descuento1 = valor*25/100;
-let descuento2 = valor*10/100
-
-alert(compra(valor));
+function disponibleJuegos(juegos){
+    let lista = " " ;
+    for (const juego of juegos){
+        lista += "el Juego"+ " " + juego.nombre+ " " + " Cuesta $"+ juego.precio
+    }
+    return lista;
 }
+
+for (let i = 0; i < 1 ; i++){
+    let filtro = stockJuegos (juegos,prompt ("Ingresar Nombre de juego a Buscar"));
+    if (filtro.length > 0){
+        alert(disponibleJuegos(filtro));
+    }else{
+        alert("Juego no encontrado")
+    }
+}}
