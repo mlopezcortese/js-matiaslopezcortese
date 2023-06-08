a5114f848035dc2bb1615b50cb80f9926ddae71e
Matias Lopez Cortese <matais.lopez.89@hotmail.com>
2023-06-08 18:12:34 -0300

    array

---
+++ stock.js
@@ -0,0 +1,20 @@
+const productos = [
+    //juegos
+    {id:1,nombre:"Elden Ring",imagen:"./imagenes/juegos/Ering",categoria:{nombre:"juegos",id:"juegos"}, precio:1000},
+    {id:2,nombre:"Dark Souls",imagen:"./imagenes/juegos/Ds1",categoria:{nombre:"juegos",id:"juegos"}, precio:2000},
+    {id:3,nombre:"Dark Souls2",imagen:"./imagenes/juegos/Ds2",categoria:{nombre:"juegos",id:"juegos"}, precio:3000},
+    {id:4,nombre:"Dark Souls3",imagen:"./imagenes/juegos/Ds3",categoria:{nombre:"juegos",id:"juegos"}, precio:4000},
+    {id:5,nombre:"Gta",imagen:"./imagenes/juegos/GtaV",categoria:{nombre:"juegos",id:"juegos"}, precio:5000},
+    //mouses
+    {id:6,nombre:"Mouse Corsair 1 " ,imagen:"./imagenes/mouse/mouse1",categoria:{nombre:"mouse",id:"mouse"},precio:2000},        
+    {id:7,nombre:"Mouse Corsair 2" ,imagen:"./imagenes/mouse/mouse2",categoria:{nombre:"mouse",id:"mouse"},precio:3000},    
+    {id:8,nombre:"Mouse Corsair 3" ,imagen:"./imagenes/mouse/mouse3",categoria:{nombre:"mouse",id:"mouse"},precio:4000},    
+    {id:9,nombre:"Mouse Corsair 4" ,imagen:"./imagenes/mousemouse4",categoria:{nombre:"mouse",id:"mouse"},precio:5000},    
+    {id:10,nombre:"Mouse Corsair 5" ,imagen:"./imagenes/mouse/mouse5",categoria:{nombre:"mouse",id:"mouse"},precio:6000},    
+    //teclados
+    {id:11,nombre:"Teclado Corsair k60",imagen:"./imagenes/teclado/k60",categoria:{nombre:"teclado",id:"teclado"},precio:5000},
+    {id:12,nombre:"Teclado Corsair K65",imagen:"./imagenes/teclado/k65",categoria:{nombre:"teclado",id:"teclado"},precio:6000},
+    {id:13,nombre:"Teclado Corsair K70",imagen:"./imagenes/teclado/k70",categoria:{nombre:"teclado",id:"teclado"},precio:7000},
+    {id:14,nombre:"Teclado Corsair K95",imagen:"./imagenes/teclado/k95",categoria:{nombre:"teclado",id:"teclado"},precio:8000},
+    {id:15,nombre:"Teclado Corsair K100",imagen:"./imagenes/teclado/k100",categoria:{nombre:"teclado",id:"teclado"},precio:9000},
+    ];
